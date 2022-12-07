2b5620e43880ebe9f20cf32d27d94ae9bf8ac49f
Dijane Azemi <dijane.azemi@student.uni-pr.edu>
2022-12-07 20:05:01 +0100

    Shtimi i javascript

--- js/index.js
+++ js/index.js
@@ -1,65 +1,108 @@
-(function (){
-    "use strict";
+(function () {
+  "use strict";
 
-    const addForm = document.getElementById("add-form");
-    const addModal = document.getElementById("add-modal");
-    const closeModalEl = document.getElementById("close-modal");
-    const productList = document.getElementById("products-section");
-    const addButtons = document.getElementsByClassName("add_button");
+  const addForm = document.getElementById("add-form");
+  const addModal = document.getElementById("add-modal");
+  const closeModalEl = document.getElementById("close-modal");
+  const productList = document.getElementById("products-section");
+  const addButtons = document.getElementsByClassName("add_button");
 
-    Array.from(addButtons).forEach(function (element){
-        element.addEventListener("click", showAddModal);
-    });
-    function showAddModal(){
-        addModal.classList.add("add_modal--shown");
+  Array.from(addButtons).forEach(function (element) {
+    element.addEventListener("click", showAddModal);
+  });
+  /*
+    function getProducts(){
+      fetch("https://dummyjson.com/products")
+      .then((response)=>response.json())
+      .then((value)=>{
+        value.products.forEach((item)=>
+        renderProduct({
+          title: item.title,
+          price: item.price,
+          thumbnail: item.thumbnail,
+          description: item.description,
+        })
+        );
+        })
+      .catch((error)=>console.error(error));
     }
 
-    closeModalEl.addEventListener("click", closeModal);
+    */
+  async function getProducts() {
+    try {
+      const response = await fetch("https://dummyjson.com/products");
 
+      const values = await response.json();
 
-    document.addEventListener("keydown", (e) => {
-        if(e.key == "Escape"){
-            closeModal();
-        }
-    });
-
-    function closeModal(){
-        addModal.classList.remove("add_modal--shown");
-        addForm.reset();
+      values.products.forEach((item) =>
+        renderProduct({
+          title: item.title,
+          price: item.price,
+          thumbnail: item.thumbnail,
+          description: item.description,
+        })
+      );
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    addForm.addEventListener("submit", function (e){
-        e.preventDefault();
+  function renderProduct(product) {
+    productList.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="products-section-item">
+          <div class="products-section-item__thumb">
+            <img src="${product.thumbnail}" alt="${product.title}" />
+          </div>
+          <div class="products-section-item__content">
+            <div class="products-section-item__headings">
+              <h3 class="products-section-item__title">
+              ${product.title}
+             </h3>
+      
+              <p class="products-section-item__description">
+              ${product.description}
+              </p>
+            </div>
+      
+            <span class="products-section-item__price">
+              ${product.price}€
+            </span>
+          </div>
+        </div>`
+    );
+  }
 
-        const formData = new FormData(e.target);
+  function showAddModal() {
+    addModal.classList.add("add_modal--shown");
+  }
 
-        productList.insertAdjacentHTML(
-            "beforeend",
-            `<div class="products-section-item">
-              <div class="products-section-item__thumb">
-                <img src="./assets/images/image-first.jpg" alt="Item Thumbnail" />
-              </div>
-              <div class="products-section-item__content">
-                <div class="products-section-item__headings">
-                  <h3 class="products-section-item__title">
-                  ${formData.get("title")}
-                  </h3>
-          
-                  <p class="products-section-item__description">
-                  ${formData.get("description")}
-                  </p>
-                </div>
-          
-                <span class="products-section-item__price">
-                  ${formData.get("price")}€
-                </span>
-              </div>
-            </div>`
-          );
-          
-          addModal.classList.remove("add_modal--shown");
+  closeModalEl.addEventListener("click", closeModal);
 
-          this.reset();
+  document.addEventListener("keydown", (e) => {
+    if (e.key == "Escape") {
+      closeModal();
+    }
+  });
+
+  function closeModal() {
+    addModal.classList.remove("add_modal--shown");
+    addForm.reset();
+  }
+
+  addForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    renderProduct({
+      title: formData.get("title"),
+      description: formData.get("description"),
+      price: formData.get("description"),
     });
 
+    addModal.classList.remove("add_modal--shown");
+
+    this.reset();
+  });
 })();
